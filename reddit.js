33691ed5706d4d51666249e9893481c2173ce8a7
Cam205 <cameronbuchanan17@yahoo.com>
2021-10-28 14:34:40 -0500

    Fixed some small things

--- reddit.js
+++ reddit.js
@@ -1,26 +1,29 @@
 const path = require('path');
 const fs = require('fs');
-const request = require('request-promise');
+const rp = require('request-promise');
 
-let articlePath = path.join(__dirname, './popular.articles.json');
+rp("https://reddit.com/r/popular.json")
+  .then((res) => {
+    let articleArr = [];
 
-request('https://reddit.com/r/popular.json', (err, res, body) => {
-    
-    if(err) console.log(err);
+    JSON.parse(res).data.children.map((article) => {
+      let obj = {
+        title: article.data.title,
+        url: article.data.url,
+        author: article.data.author,
+      };
 
-    let articlesArr = [];
-
-    JSON.parse(body).data.children.forEach(item => {
-
-        let title = item.data.title;
-        let url = item.data.url;
-        let author = item.data.author
-
-
-        let articleObj = {title, url, author};
-
-        articlesArr.push(articleObj);
-    })
-
-        fs.appendFileSync(articlePath, JSON.stringify(articlesArr));
-    })
+      articleArr.push(obj);
+      fs.writeFile(
+        "popular-articles.json",
+        JSON.stringify(articleArr),
+        (err) => {
+          if (err) throw err;
+        }
+      );
+      console.log(obj);
+    });
+  })
+  .catch((err) => {
+    if (err) throw err;
+  });
